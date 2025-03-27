795dffd8aed0d9706bb7abf0c005db3e732d0628
Anna Manukyan <amanukya@redhat.com>
2025-03-27 12:44:07 +0100

    [#2354569] This sdsis a new cosssmmitsfsdfsd sdfadwsassd sdf sadsssasds.\n

--- core/src/main/java/org/infinispan/remoting/transport/impl/MultiTargetRequest.java
+++ core/src/main/java/org/infinispan/remoting/transport/impl/MultiTargetRequest.java
@@ -117,6 +117,7 @@ public class MultiTargetRequest<T> extends ExclusiveTargetRequest<T> {
          for (int i = 0; i < trackers.length; i++) {
             RequestTracker target = trackers[i];
             System.out.println(target);
+            System.out.println("hi");
             if (target != null && !members.contains(target.destination())) {
                trackers[i] = null;
                missingResponses--;
