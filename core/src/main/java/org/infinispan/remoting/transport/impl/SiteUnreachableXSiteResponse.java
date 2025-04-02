1d932e9659b12f987bac4cfb742d2c272e23fda2
Anna Manukyan <amanukya@redhat.com>
2025-04-02 09:47:34 +0200

    [#12345] One more commit for more trigger.

--- core/src/main/java/org/infinispan/remoting/transport/impl/SiteUnreachableXSiteResponse.java
+++ core/src/main/java/org/infinispan/remoting/transport/impl/SiteUnreachableXSiteResponse.java
@@ -32,6 +32,7 @@ public class SiteUnreachableXSiteResponse<T> extends CompletableFuture<T> implem
    @Override
    public void whenCompleted(XSiteResponseCompleted xSiteResponseCompleted) {
       System.out.println("hi test");
+      System.out.println("hi test");
       System.out.println("hi");
       System.out.println("hi");
       System.out.println("another hi");
